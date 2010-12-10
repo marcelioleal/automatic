c83d304395b086fdad749aa412d697a11db65ded
Marcelio Leal <marcelioleal@gmail.com>
2010-12-10 01:46:44 -0300

    #issue/1

--- public/index.php
+++ public/index.php
@@ -8,6 +8,10 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
 
+// Define path to application directory
+defined('BASE_URL')
+    || define('BASE_URL', '/automatic/public/');
+
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
