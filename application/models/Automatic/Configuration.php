67ab7ace5df29ef60b22ab7d3ef7cf44842f2521
Marcelio Leal <marcelioleal@gmail.com>
2010-12-10 03:46:57 -0300

    general configuration

---
+++ application/models/Automatic/Configuration.php
@@ -0,0 +1,47 @@
+<?php
+namespace Automatic;
+
+class Configuration{
+    
+    private $_pathConfig;
+    private $_generalConfigName;
+    private $_generalConfig;
+    
+    function __construct(){
+        $this->_pathConfig = APPLICATION_PATH.'/configs/';
+        $this->_generalConfigName = "application.ini";
+        $this->setGeneralConfiguration();
+    }
+    
+    public function setGeneralConfiguration(){
+        $this->_generalConfig = new \Zend\Config\Ini($this->_pathConfig.$this->_generalConfigName,null,array('skipExtends'=> true,"allowModifications"=>true));
+    }
+    
+    public function getParam($param){
+        try{
+            eval(str_replace(".","->",'$value = $this->_generalConfig->'.APPLICATION_ENV.'->'.$param).";");
+            if(!$value)
+                eval(str_replace(".","->",'$value = $this->_generalConfig->production->'.$param).';');
+            return $value;
+        }catch (Exception $e){
+            print $e->getMessage();
+        }
+    }
+    
+    public function setParam($param,$value){
+        try{
+            print $value;
+            eval(str_replace(".","->",'$this->_generalConfig->'.APPLICATION_ENV.'->'.$param).' = $value;');
+            //if(!$value)
+                eval(str_replace(".","->",'$this->_generalConfig->production->'.$param).'= $value;');
+        }catch (Exception $e){
+            print $e->getMessage();
+        }
+    }
+    
+    public function write(){
+        $configWriter = new \Zend\Config\Writer\Ini();
+        $configWriter->write($this->_pathConfig.$this->_generalConfigName,$this->_generalConfig);
+    }
+    
+}
